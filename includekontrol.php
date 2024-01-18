8e3315eb024f7a077aac794607c0c20f5f9fad31
Emre SAKA <94871056+EmreDepo@users.noreply.github.com>
2024-01-18 22:51:23 +0300

    Create includekontrol.php
    İnclude içine ekleninilen dbconnection kontrolü

---
+++ includekontrol.php
@@ -0,0 +1,15 @@
+
+<html>
+<body>
+
+<?php
+// "inc" dizini altındaki "grey.php" dosyasını dahil et
+include('inc/grey.php');
+
+// Eğer "grey.php" dosyası başarıyla dahil edildiyse, bağlantı kurulmuş demektir
+echo "Bağlantı başarıyla sağlandı!";
+?>
+
+
+</body>
+</html>
